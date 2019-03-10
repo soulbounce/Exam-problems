631e1d9fa2250b26366a92c63036f5c4977175a8
Boyan Antonov <boyanantonov@gmail.com>
2019-03-10 22:08:30 +0200

    Add files via upload
    Exam problem - Old exams - Covering everything in Softuni's Programming Basics - C# + new lessons from Technology Fundamentals with C# -  Arrays/Lists/Methods

---
+++ 2. Present delivery/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Present_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> santasList = Console.ReadLine().Split('@').Select(int.Parse).ToList();
+
+            int position = 0;
+            int count = 0;
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command[0] == "Merry")
+                {
+                    break;
+                }
+                if (command[0] == "Jump")
+                {
+                    int jump = int.Parse(command[1]);
+
+                    position = (position + jump) % santasList.Count;
+
+                    if (santasList[position] == 0)
+                    {
+                        Console.WriteLine($"House {position} will have a Merry Christmas.");
+                    }
+                    else
+                    {
+                        santasList[position] -= 2;
+                    }
+                }
+            }
+            Console.WriteLine($"Santa's last position was {position}.");
+
+            foreach (var item in santasList)
+            {
+                if (item > 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine($"Mission was successful.");
+            }
+            else
+            {
+                Console.WriteLine($"Santa has failed {count} houses.");
+            }
+        }
+    }
+}
