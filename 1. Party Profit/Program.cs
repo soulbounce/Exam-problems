60cf57d51b71b84dce7e831249aac1ccad08a4b1
Boyan Antonov <boyanantonov@gmail.com>
2019-03-10 22:04:52 +0200

    Add files via upload
    Exam problem - Old exams - Covering everything in Softuni's Programming Basics - C# + new lessons from Technology Fundamentals with C# -  Arrays/Lists/Methods

---
+++ 1. Party Profit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _1._Party_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int party = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int currentCoins = 0;
+
+            for (int day = 1; day <= days; day++)
+            {
+                if (day % 10 == 0)
+                {
+                    party -= 2;
+                }
+                if (day % 15 == 0)
+                {
+                    party += 5;
+                }
+                currentCoins += 50;
+                currentCoins -= 2 * party;
+                if(day % 3 == 0)
+                {
+                    currentCoins -= 3 * party;
+                    if(day % 5 == 0)
+                    {
+                        currentCoins -= 2 * party;
+                    }
+                }
+                if(day % 5 == 0)
+                {
+                    currentCoins += 20 * party;
+                }
+            }
+            int coinsPerCompanion = currentCoins / party;
+            Console.WriteLine($"{party} companions received {coinsPerCompanion} coins each.");
+        }
+    }
+}
