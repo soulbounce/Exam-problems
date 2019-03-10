bc6f2a859ac6dcfcab911b1f5e27b9238ea2bfb6
Boyan Antonov <boyanantonov@gmail.com>
2019-03-10 22:07:41 +0200

    Add files via upload
    Exam problem - Old exams - Covering everything in Softuni's Programming Basics - C# + new lessons from Technology Fundamentals with C# -  Arrays/Lists/Methods

---
+++ 2. Dungeonest Dark/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_2._Dungeonest_Dark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> dungeonRooms = Console.ReadLine().Split('|').ToList();
+
+            int currentHP = 100;
+            int currentGold = 0;
+
+            for (int i = 0; i < dungeonRooms.Count; i++)
+            {
+                string[] room = dungeonRooms[i].Split();
+
+                if(room[0] == "potion")
+                {
+                    int potion = int.Parse(room[1]);
+
+                    if(currentHP + potion > 100)
+                    {
+                        int amountHealed = 100 - currentHP;
+                        Console.WriteLine($"You healed for {amountHealed} hp.");
+                        currentHP = 100;
+                    }
+                    else
+                    {
+                        currentHP += potion;
+                        Console.WriteLine($"You healed for {potion} hp.");
+                    }
+                    Console.WriteLine($"Current health: {currentHP} hp.");
+                }
+                else if(room[0] == "chest")
+                {
+                    int goldFound = int.Parse(room[1]);
+                    currentGold += goldFound;
+                    Console.WriteLine($"You found {goldFound} coins.");
+                }
+                else
+                {
+                    string monster = room[0];
+                    int monsterAttack = int.Parse(room[1]);
+
+                    if(currentHP - monsterAttack <= 0)
+                    {
+                        Console.WriteLine($"You died! Killed by {monster}.");
+                        Console.WriteLine($"Best room: {i+1}");
+                        return;
+                    }
+                    else
+                    {
+                        currentHP -= monsterAttack;
+                        Console.WriteLine($"You slayed {monster}.");
+                    }
+                }
+            }
+            Console.WriteLine($"You've made it!");
+            Console.WriteLine($"Coins: {currentGold}");
+            Console.WriteLine($"Health: {currentHP}");
+        }
+    }
+}
